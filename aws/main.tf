b2bf6a1557fce4b4456ff45288b2c647b645fb66
Ludovic Boutros <lboutros@confluent.io>
2023-05-05 14:05:29 +0200

    Spread servers through different AZ

--- aws/main.tf
+++ aws/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = var.aws_region
+  region                   = var.aws_region
   shared_credentials_files = var.aws_credential_files
 }
 
 data "aws_key_pair" "my_key_pair" {
-  key_name   = var.aws_key_pair
+  key_name = var.aws_key_pair
 }
 
 data "aws_security_group" "security-group" {
@@ -13,6 +13,10 @@ data "aws_security_group" "security-group" {
 
 data "aws_route53_zone" "zone" {
   name = var.aws_zone
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 resource "aws_route53_record" "broker_record" {
@@ -76,108 +80,113 @@ resource "aws_route53_record" "connect_record" {
 }
 
 resource "aws_instance" "zk" {
-  ami           = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
-  instance_type = "t2.large"
-  count         = var.zookeeper_count
-  key_name      = data.aws_key_pair.my_key_pair.key_name
+  ami                    = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
+  instance_type          = "t2.large"
+  count                  = var.zookeeper_count
+  key_name               = data.aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [data.aws_security_group.security-group.id]
-
+  availability_zone      = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
   root_block_device {
     volume_size = 20
   }
 
   tags = {
-    Name = "${var.resource_name_prefix}-zk-${count.index}"
-    DnsName = "zk${count.index}.${var.aws_zone}"
-    SubDomain = "zk${count.index}"
-    DN = "zk${count.index}"
+    Name      = "${var.resource_name_prefix}-zk-${count.index}"
+    DnsName   = "zk-${count.index}.${var.aws_zone}"
+    SubDomain = "zk-${count.index}"
+    DN        = "zk-${count.index}"
   }
 }
 
 resource "aws_instance" "broker" {
-  ami           = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
-  instance_type = "t2.large"
-  count         = var.broker_count
-  key_name      = data.aws_key_pair.my_key_pair.key_name
+  ami                    = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
+  instance_type          = "t2.large"
+  count                  = var.broker_count
+  key_name               = data.aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [data.aws_security_group.security-group.id]
+  availability_zone      = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
 
   root_block_device {
     volume_size = 20
   }
 
   tags = {
-    Name = "${var.resource_name_prefix}-broker-${count.index}"
-    DnsName = "broker${count.index}.${var.aws_zone}"
-    SubDomain = "broker${count.index}"
-    DN = "broker"
+    Name      = "${var.resource_name_prefix}-broker-${count.index}"
+    DnsName   = "broker-${count.index}.${var.aws_zone}"
+    SubDomain = "broker-${count.index}"
+    DN        = "broker"
   }
 }
 
 resource "aws_instance" "sr" {
-  ami           = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
-  instance_type = "t2.large"
-  count         = var.registry_count
-  key_name      = data.aws_key_pair.my_key_pair.key_name
+  ami                    = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
+  instance_type          = "t2.large"
+  count                  = var.registry_count
+  key_name               = data.aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [data.aws_security_group.security-group.id]
+  availability_zone      = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
 
   tags = {
-    Name = "${var.resource_name_prefix}-sr-${count.index}"
-    DnsName = "sr${count.index}.${var.aws_zone}"
-    SubDomain = "sr${count.index}"
-    DN = "sr${count.index}"
+    Name      = "${var.resource_name_prefix}-sr-${count.index}"
+    DnsName   = "sr-${count.index}.${var.aws_zone}"
+    SubDomain = "sr-${count.index}"
+    DN        = "sr-${count.index}"
   }
 }
 
 resource "aws_instance" "kc" {
-  ami           = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
-  instance_type = "t2.large"
-  count         = var.ksqldb_count
-  key_name      = data.aws_key_pair.my_key_pair.key_name
+  ami                    = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
+  instance_type          = "t2.large"
+  count                  = var.ksqldb_count
+  key_name               = data.aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [data.aws_security_group.security-group.id]
+  availability_zone      = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
 
   tags = {
-    Name = "${var.resource_name_prefix}-kc-${count.index}"
-    DnsName = "kc${count.index}.${var.aws_zone}"
-    SubDomain = "kc${count.index}"
-    DN = "kc${count.index}"
+    Name      = "${var.resource_name_prefix}-kc-${count.index}"
+    DnsName   = "kc-${count.index}.${var.aws_zone}"
+    SubDomain = "kc-${count.index}"
+    DN        = "kc-${count.index}"
   }
 }
 
 resource "aws_instance" "cc" {
-  ami           = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
-  instance_type = "t2.large"
-  count         = var.ccc_count
-  key_name      = data.aws_key_pair.my_key_pair.key_name
+  ami                    = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
+  instance_type          = "t2.large"
+  count                  = var.ccc_count
+  key_name               = data.aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [data.aws_security_group.security-group.id]
-  
+  availability_zone      = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
+
   root_block_device {
     volume_size = 20
   }
-  
+
   tags = {
-    Name = "${var.resource_name_prefix}-cc-${count.index}"
-    DnsName = "cc${count.index}.${var.aws_zone}"
-    SubDomain = "cc${count.index}"
-    DN = "cc${count.index}"
+    Name      = "${var.resource_name_prefix}-cc-${count.index}"
+    DnsName   = "cc-${count.index}.${var.aws_zone}"
+    SubDomain = "cc-${count.index}"
+    DN        = "cc-${count.index}"
   }
 }
 
 resource "aws_instance" "connect" {
-  ami           = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
-  instance_type = "t2.large"
-  count         = var.connect_count
-  key_name      = data.aws_key_pair.my_key_pair.key_name
+  ami                    = "ami-0cc4e06e6e710cd94" # Ubuntu 20.04 LTS
+  instance_type          = "t2.large"
+  count                  = var.connect_count
+  key_name               = data.aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [data.aws_security_group.security-group.id]
+  availability_zone      = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
 
   root_block_device {
     volume_size = 20
   }
 
   tags = {
-    Name = "${var.resource_name_prefix}-connect-${count.index}"
-    DnsName = "connect${count.index}.${var.aws_zone}"
-    SubDomain = "connect${count.index}"
-    DN = "connect${count.index}"
+    Name      = "${var.resource_name_prefix}-connect-${count.index}"
+    DnsName   = "connect-${count.index}.${var.aws_zone}"
+    SubDomain = "connect-${count.index}"
+    DN        = "connect-${count.index}"
   }
 }
 
@@ -189,6 +198,6 @@ output "aws_nameservers" {
 output "all_hosts_join" {
   value = join("\n", [
     for instance in setunion(aws_instance.cc, aws_instance.kc, aws_instance.sr, aws_instance.broker, aws_instance.zk) :
-    format("%s#%s#%s#%s", instance.tags.DnsName,instance.private_dns,instance.private_ip, instance.tags.DN)
+    format("%s#%s#%s#%s", instance.tags.DnsName, instance.private_dns, instance.private_ip, instance.tags.DN)
   ])
 }
