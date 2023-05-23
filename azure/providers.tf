b8a7b6fc195abafcb9dca604939166f2365f65a6
Victor Cosqui <vcosqui@confluent.io>
2023-05-23 08:03:36 +0200

    added tf state backend in azure with external backend settings, run with
    ```tf init -backend-config "resource_group_name=migrations" \
     -backend-config "storage_account_name=migrationstfstateazure" \
     -backend-config "container_name=tfstate" \
     -backend-config "key=terraform.tfstate"```

--- azure/providers.tf
+++ azure/providers.tf
@@ -1,9 +1,5 @@
 terraform {
   required_version = ">=1.0"
   backend "azurerm" {
-      resource_group_name  = "migrations"
-      storage_account_name = "migrationstfstateazure"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
   }
 }
